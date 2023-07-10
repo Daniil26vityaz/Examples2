94ae2a8af2d0bde1e01d1e32b5c1a19c4e900229
Daniil <mr.varfolomeev@bk.ru>
2023-07-10 19:46:03 +0300

    Added conditions

---
+++ Examples005_cycles_with_conditions/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username == "Daniil"){
+    Console.WriteLine("COOOL!! Nice to meet you, sir :=)");
+}
+else{
+    Console.WriteLine("Wellcome!!");
+}
